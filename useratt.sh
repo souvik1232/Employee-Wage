acb2b2b804ebaca08eb7b1bce1d73f88d6f08d80
souvik1232 <souvik199831@gmail.com>
2020-11-17 08:39:26 +0530

    Added : The basic user attendance

---
+++ useratt.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+isPresent=1
+random=$(( RANDOM%2 ))
+echo "Random number: "$random
+if(( $random == $isPresent ))
+then
+echo "Employee is Present!"
+else
+echo "Employee is absent!"
+fi
