27ff80acf5e7c7ef8cd443b7120e103362e99258
souvik1232 <souvik199831@gmail.com>
2020-11-17 12:10:13 +0530

    Refractored:work hours function added

--- useratt.sh
+++ useratt.sh
@@ -1,25 +1,38 @@
 #! /bin/bash
 IS_PART_TIME=1
 IS_FULL_TIME=2
+MAX_HRS_IN_MONTH=100
 EMP_RATE_PER_HR=20
-MAX_HRS_IN_MONTH=10
 NUM_WORKING_DAYS=20
 totalEmpHrs=0
 totalWorkingDays=0
-while(( $totalEmpHrs < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
-do
-	((totalWorkingDays++))
-	echo "Total working day: $totalWorkingDays"
-	random=$(( RANDOM%3 ))
-	case $random in
-		$IS_PART_TIME) empHrs=4
+totalWorkHours=0
+function calculateDailyWage()
+{
+	empHrs=$1
+	daySalary=$(( $empHrs * $EMP_RATE_PER_HR ))
+	echo $daySalary
+}
+
+function getWorkingHours()
+{
+	case $1 in
+		$IS_FULL_TIME ) empHrs=8
 			;;
-		$IS_FULL_TIME) empHrs=8
+		$IS_PART_TIME ) empHrs=4
 			;;
 		*) empHrs=0
+			;;
 	esac
-	totalEmpHrs=$(( $totalEmpHrs + $empHrs ))
-	echo "Total Employee Hours: $totalEmpHrs"
+	echo $empHrs
+}
+
+while(( $totalWorkHours < $MAX_HRS_IN_MONTH && $totalWorkingDays < $NUM_WORKING_DAYS ))
+do
+	((totalWorkingDays++))
+
+	workHours="$( getWorkingHours $(( RANDOM%3 )) )"
+	echo "Work hours from function getWorkingHours()::::::::::>>> $workHours "
+	totalWorkHours=$(( $totalWorkHours + $workHours ))
+	echo -e "TotalWorkHours: $totalWorkHours\n"
 done
-totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR))
-echo "Total Salary: $totalSalary"
